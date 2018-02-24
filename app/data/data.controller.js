df2715415a1545b22b155d6dce576032964bc610
unknown <bartosz.boruta@hewalex.pl>
2018-02-24 23:29:55 +0100

    added mocked dashboard routing

--- app/data/data.controller.js
+++ app/data/data.controller.js
@@ -4,6 +4,62 @@ var Data = require('./data.model');
 var Device = require('../device/device.model');
 var Status = require('../status/status.model');
 
+exports.index = function (req, res) {
+
+    const data =  [
+        {
+            id: 'lastSwitchOn',
+            title: 'Last switch on',
+            subtitle: '',
+            additionalInfo: 'test',
+            additionalInfo2: 'test2',
+            backgroundColor: '#177F8A',
+            icon: 'ios-analytics'
+        },
+        {
+            id: 'lastSwitchOn',
+            title: 'Last switch on',
+            subtitle: '',
+            additionalInfo: 'test',
+            additionalInfo2: '2017-02-19 22:12:00',
+            backgroundColor: '#4C8DCA',
+            icon: 'ios-archive'
+        },
+        {
+            id: 'lastSwitchOn',
+            title: 'Last switch on',
+            subtitle: '',
+            additionalInfo: 'test',
+            additionalInfo2: 'test2',
+            backgroundColor: '#0CEBC7',
+            icon: 'ios-bulb-outline'
+        },
+        {
+            id: 'lastSwitchOn',
+            title: 'Last switch on',
+            subtitle: '',
+            additionalInfo: 'test',
+            additionalInfo2: 'test2',
+            backgroundColor: '#385DCA',
+            icon: 'ios-flame-outline'
+        },
+    ];
+
+    res.status(200).send(data);
+
+
+        // Device.find({
+    //     createdBy: req.user.id
+    // }).populate('status', 'data').exec( function (err, device) {
+    //     if (err) {
+    //         return res.status(500).send("There was a problem finding the device.");
+    //     }
+    //     if (!device) {
+    //         return res.status(404).send("No device found.");
+    //     }
+    //     res.status(200).send(device);
+    // });
+};
 
 /*
 * Sets new status in status schema and adds to data
