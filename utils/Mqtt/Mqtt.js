dc771d095b3b19f408c05196db0e7c20106a76d0
bartoszboruta <bboruta.dev@gmail.com>
2018-09-17 00:11:29 +0200

    wip

--- utils/Mqtt/Mqtt.js
+++ utils/Mqtt/Mqtt.js
@@ -1,27 +1,34 @@
-var mqtt = require('mqtt');
+var mqtt = require('mqtt')
 
 module.exports = class Mqtt {
-    publish(device, data, callback) {
-        if (!device || !device.serial || !device.password) {
-            return callback(false);
-        }
-
-        var options = {
-            port: process.env.MQTT_BROKER_PORT,
-            username: device.serial.toString(),
-            password: device.password,
-            protocolId: 'MQTT',
-            protocolVersion: 4,
-            clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8)
-        };
-
-        var client = mqtt.connect(process.env.MQTT_BROKER_URL, options);
-
-        client.on('connect', function() {
-            client.publish(device.serial.toString(), data, function() {
-                client.end();
-                callback(true);
-            })
-        })
+  publish(device, data, callback) {
+    if (!device || !device.serial || !device.password) {
+      return callback(false)
     }
-};
+
+    var options = {
+      port: process.env.MQTT_BROKER_PORT,
+      username: device.serial.toString(),
+      password: device.password,
+      protocolId: 'MQTT',
+      protocolVersion: 4,
+      clientId:
+        'mqttjs_' +
+        Math.random()
+          .toString(16)
+          .substr(2, 8),
+    }
+
+    var client = mqtt.connect(
+      process.env.MQTT_BROKER_URL,
+      options,
+    )
+
+    client.on('connect', function() {
+      client.publish(device.serial.toString() + '_tt', data, function() {
+        client.end()
+        callback(true)
+      })
+    })
+  }
+}
